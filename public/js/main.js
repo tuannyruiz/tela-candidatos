dea87220cd89b61ec30fdae8d19f6695ccb075cc
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-05-09 20:59:56 -0300

    criação da função que checa se o candidato é novo

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,16 @@ const container = document.createElement('div');
 container.setAttribute('class', 'container');
 app.appendChild(container);
 
+function newCandidate(person) {
+  if(person.newThisWeek == true) {
+    return '<p class="new">Novo esta semana</p>'
+  } else {
+    return '';
+  }
+}
+
 function createCard(person) {
-  return '<img src="' + person.imageUrl + '" class="photo"><div class="profile"><h1 class="candidate">' + person.name +'</h1><h2 class="occupation">' + person.career + '</h2><ul class="info"><li class="experiences"><img src="images/work-icn.svg" alt="Ícone que representa experiências profissionais">' + person.lastCompanies +'</li><li class="university"><img src="images/study-icn.svg" alt="Ícone que representa univeridade">' + person.university + '</li><li class="local"><img src="images/location-icn.svg" alt="Ícone que representa a localização"> Disposto a trabalhar em: ' + person.cities + '</li><li class="skills"><img src="images/skills-icn.svg" alt="Ícone que representa habilidades"> Principais habilidades: ' + person.mainSkills + '</li></ul></div><button class="favorite"><img src="images/star.svg" alt=""></button><button class="more"><img src="images/dots.svg" alt=""></button>';
+  return '<img src="' + person.imageUrl + '" class="photo"><div class="profile"><h1 class="candidate">' + person.name +'</h1>' + newCandidate(person) + '<h2 class="occupation">' + person.career + '</h2><ul class="info"><li class="experiences"><img src="images/work-icn.svg" alt="Ícone que representa experiências profissionais">' + person.lastCompanies +'</li><li class="university"><img src="images/study-icn.svg" alt="Ícone que representa univeridade">' + person.university + '</li><li class="local"><img src="images/location-icn.svg" alt="Ícone que representa a localização"> Disposto a trabalhar em: ' + person.cities + '</li><li class="skills"><img src="images/skills-icn.svg" alt="Ícone que representa habilidades"> Principais habilidades: ' + person.mainSkills + '</li></ul></div><button class="favorite"><img src="images/star.svg" alt=""></button><button class="more"><img src="images/dots.svg" alt=""></button>';
   
 }
 
