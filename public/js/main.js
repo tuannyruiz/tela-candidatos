80261b3e174b6d8c9d843a87e1ab9af240493f29
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-05-09 20:42:11 -0300

    adiciona as informações da api ao template

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,12 @@
 const app = document.getElementById('root');
-const card = document.createElement('div');
-card.setAttribute('class', 'card');
-app.appendChild(card);
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+app.appendChild(container);
 
+function createCard(person) {
+  return '<img src="' + person.imageUrl + '" class="photo"><div class="profile"><h1 class="candidate">' + person.name +'</h1><h2 class="occupation">' + person.career + '</h2><ul class="info"><li class="experiences"><img src="images/work-icn.svg" alt="Ícone que representa experiências profissionais">' + person.lastCompanies +'</li><li class="university"><img src="images/study-icn.svg" alt="Ícone que representa univeridade">' + person.university + '</li><li class="local"><img src="images/location-icn.svg" alt="Ícone que representa a localização"> Disposto a trabalhar em: ' + person.cities + '</li><li class="skills"><img src="images/skills-icn.svg" alt="Ícone que representa habilidades"> Principais habilidades: ' + person.mainSkills + '</li></ul></div><button class="favorite"><img src="images/star.svg" alt=""></button><button class="more"><img src="images/dots.svg" alt=""></button>';
+  
+}
 
 var request = new XMLHttpRequest();
 request.open('GET', 'http://localhost:3000/candidates', true);
@@ -12,7 +16,10 @@ request.onload = function () {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach(person => {
-      console.log(person.name);
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+      container.appendChild(card);
+      card.insertAdjacentHTML('beforeend', createCard(person));
     });
   } else {
     console.log('error');
